c25ce0607fed70b93cb7fb5f2599299ddde08128
ReyesGutierrez <backtgh@gmail.com>
2022-05-20 12:54:11 -0500

    first commit

---
+++ vpms/admin/includes/sidebar.php
@@ -0,0 +1,42 @@
+ <aside id="left-panel" class="left-panel">
+     <nav class="navbar navbar-expand-sm navbar-default">
+         <div id="main-menu" class="main-menu collapse navbar-collapse">
+             <ul class="nav navbar-nav">
+                 <li class="active">
+                     <a href="dashboard.php"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                 </li>
+
+
+                 <li class="menu-item-has-children dropdown">
+                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tipo de vehiculo</a>
+                     <ul class="sub-menu children dropdown-menu">
+                         <li><i class="fa fa-table"></i><a href="add-category.php">Añadir tipo de vehículo</a></li>
+                         <li><i class="fa fa-table"></i><a href="manage-category.php">Editar los tipos de vehículos</a></li>
+                     </ul>
+                 </li>
+                 <li>
+                     <a href="add-vehicle.php"> <i class="menu-icon ti-email"></i>Añadir vehículo</a>
+                 </li>
+                 <li class="menu-item-has-children dropdown">
+                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Vehículos</a>
+                     <ul class="sub-menu children dropdown-menu">
+                         <li><i class="menu-icon fa fa-th"></i><a href="manage-incomingvehicle.php">Entrada de vehículo</a></li>
+                         <li><i class="menu-icon fa fa-th"></i><a href="manage-outgoingvehicle.php">Salida de vehículo</a></li>
+
+                     </ul>
+                 </li>
+                 <li class="menu-item-has-children dropdown">
+                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Reports</a>
+                     <ul class="sub-menu children dropdown-menu">
+                         <li><i class="menu-icon fa fa-th"></i><a href="bwdates-report-ds.php">Between Dates Reports</a></li>
+                     </ul>
+                 </li>
+                 <li>
+                     <a href="search-vehicle.php"> <i class="menu-icon fa fa-search"></i> Buscar vehiculo</a>
+                 </li>
+                 <li>
+                     <a href="reg-users.php"> <i class="menu-icon ti-user"></i>Reg Users </a>
+                 </li>
+             </ul>
+         </div>
+ </aside>
---
+++ vpms/users/includes/footer.php
@@ -0,0 +1,10 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                   
+                    <div class="col-sm-12 text-center">
+                        Sistema de estacionamiento
+                    </div>
+                </div>
+            </div>
+        </footer>
