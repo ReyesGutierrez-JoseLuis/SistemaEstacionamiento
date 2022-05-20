32c441ebab9e5620ad09c000b7fa03697ad5176b
ReyesGutierrez <backtgh@gmail.com>
2022-05-20 16:23:48 -0500

    connection

--- estacionamiento/users/includes/dbconnection.php
+++ estacionamiento/users/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "vpmsdb");
+$con=mysqli_connect("localhost", "root", "", "parking");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
